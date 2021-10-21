7362bf5f311502e3ab886d1a439584d84e5438b4
donnypm <donnypramudia04@gmail.com>
2021-10-21 11:05:42 +0700

    update cart and style

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,12 @@
 import "../styles/globals.scss";
 import { Provider } from "react-redux";
 import store from "../redux/store/store";
-import Layout from "./components/Layout";
-import Navbar from "./components/Navbar";
+import Nav from "./components/Nav";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
-      <Navbar />
+      <Nav />
       <Component {...pageProps} />
     </Provider>
   );
